8110acfa102fabc72ba72e0815930f28e7ce52db
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-06-15 13:10:24 +0300

    Init

---
+++ src/main/java/com/volunteers/matching/VolunteerMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.volunteers.matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerMatchingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VolunteerMatchingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/volunteers/matching/VolunteerMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteers.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerMatchingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
